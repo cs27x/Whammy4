ac9f7a93f2b6fcda2a90b8307bd4bc42296fd894
Jejo Koola <jdkoola@gmail.com>
2014-10-14 11:21:56 -0500

    adding test skeleton

---
+++ src/test/java/org/magnum/cs278/testdriven/AppClassFirstSeven.java
@@ -0,0 +1,14 @@
+package org.magnum.cs278.testdriven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppClassFirstSeven {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
