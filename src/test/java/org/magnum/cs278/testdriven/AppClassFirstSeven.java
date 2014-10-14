30e1a0b5b9b833e0afb1952bf4b9aaf7b3332e14
smoreilly <smoreilly112@gmail.com>
2014-10-14 12:09:42 -0500

    hello

--- src/test/java/org/magnum/cs278/testdriven/AppClassFirstSeven.java
+++ src/test/java/org/magnum/cs278/testdriven/AppClassFirstSeven.java
@@ -5,17 +5,19 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import org.junit.Test;
+import org.magnum.cs278.testdriven.App;
 import org.magnum.cs278.testdriven.Event;
 
 public class AppClassFirstSeven {
 
+	App app = new App();
 	@Test
-	public void testAttendanceGreaterThanFive() {
+	public void testAttendanceGreaterThanFive() throws Exception {
 		List<Event> events = app.AttendanceGreaterThanFive();
 		assertTrue(events.size() > 0);
 		for(Event event : events){
-			// assertTrue(Integer.parseInt(event.getAttendance()) > 5)
-			System.err.println(event.getAttendance());
+		 assertTrue(Integer.parseInt(event.getAttendance()) > 5);
+			// System.err.println(event.getAttendance());
 		}
 	}
 		
